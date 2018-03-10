843a3d81662c7f74a920b776d6dcb5eabe33af11
Hisham Ismail <mhishami@gmail.com>
2018-03-10 15:51:41 +0800

    Added MEXC token contract for testing

---
+++ contracts/MEXCToken.sol
@@ -0,0 +1,309 @@
+/**
+ *
+ * MIT License
+ *
+ * Author: Hisham Ismail <mhishami@gmail.com>
+ * Copyright (c) 2018, MEXC Program Developers & OpenZeppelin Project.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+pragma solidity ^0.4.18;
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract Ownable {
+  address public owner;
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  function Ownable() public {
+    owner = msg.sender;
+  }
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+}
+
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+  uint256 totalSupply_;
+
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+contract StandardToken is ERC20, BasicToken {
+  mapping (address => mapping (address => uint256)) internal allowed;
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+}
+
+contract MintableToken is StandardToken, Ownable {
+  event Mint(address indexed to, uint256 amount);
+  event MintFinished();
+  bool public mintingFinished = false;
+
+  modifier canMint() {
+    require(!mintingFinished);
+    _;
+  }
+  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+    totalSupply_ = totalSupply_.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    Transfer(address(0), _to, _amount);
+    return true;
+  }
+  function finishMinting() onlyOwner canMint public returns (bool) {
+    mintingFinished = true;
+    MintFinished();
+    return true;
+  }
+}
+
+contract Destructible is Ownable {
+
+  function Destructible() public payable { }
+
+  function destroy() onlyOwner public {
+    selfdestruct(owner);
+  }
+  function destroyAndSend(address _recipient) onlyOwner public {
+    selfdestruct(_recipient);
+  }
+}
+
+contract MEXCToken is MintableToken, Destructible  {
+
+  string  public name = 'MEX Care Token';
+  string  public symbol = 'MEXC';
+  uint8   public decimals = 18;
+  uint256 public maxSupply = 1714285714 ether;    // max allowable minting.
+  bool    public transferDisabled = true;         // disable transfer init.
+
+  event Confiscate(address indexed offender, uint256 value);
+
+  // empty constructor
+  function MEXCToken() public {}
+
+  /*
+   * the real reason for quarantined addresses are for those who are
+   * mistakenly sent the MEXC tokens to the wrong address. We can disable
+   * the usage of the MEXC tokens here.
+   */
+  mapping(address => bool) quarantined;           // quarantined addresses
+  mapping(address => bool) gratuity;              // locked addresses for owners
+
+  modifier canTransfer() {
+    if (msg.sender == owner) {
+      _;
+    } else {
+      require(!transferDisabled);
+      require(quarantined[msg.sender] == false);  // default bool is false
+      require(gratuity[msg.sender] == false);     // default bool is false
+      _;
+    }
+  }
+
+  /*
+   * Allow the transfer of token to happen once listed on exchangers
+   */
+  function allowTransfers() onlyOwner public returns (bool) {
+    transferDisabled = false;
+    return true;
+  }
+
+  function disallowTransfers() onlyOwner public returns (bool) {
+    transferDisabled = true;
+    return true;
+  }
+
+  function quarantineAddress(address _addr) onlyOwner public returns (bool) {
+    quarantined[_addr] = true;
+    return true;
+  }
+
+  function unQuarantineAddress(address _addr) onlyOwner public returns (bool) {
+    quarantined[_addr] = false;
+    return true;
+  }
+
+  function lockAddress(address _addr) onlyOwner public returns (bool) {
+    gratuity[_addr] = true;
+    return true;
+  }
+
+  function unlockAddress(address _addr) onlyOwner public returns (bool) {
+    gratuity[_addr] = false;
+    return true;
+  }
+
+  /**
+   * This is confiscate the money that is sent to the wrong address accidentally.
+   * the confiscated value can then be transferred to the righful owner. This is
+   * especially important during ICO where some are *still* using Exchanger wallet
+   * address, instead of their personal address.
+   */
+  function confiscate(address _offender) onlyOwner public returns (bool) {
+    uint256 all = balances[_offender];
+    require(all > 0);
+
+    balances[_offender] = balances[_offender].sub(all);
+    balances[msg.sender] = balances[msg.sender].add(all);
+    Confiscate(_offender, all);
+    return true;
+  }
+
+  /*
+   * @dev Function to mint tokens
+   * @param _to The address that will receive the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+    require(totalSupply_ <= maxSupply.add(_amount));
+    return super.mint(_to, _amount);
+  }
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) canTransfer public returns (bool) {
+    return super.transfer(_to, _value);
+  }
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(address _from, address _to, uint256 _value) canTransfer public returns (bool) {
+    return super.transferFrom(_from, _to, _value);
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) canTransfer public returns (bool) {
+    return super.approve(_spender, _value);
+  }
+}
